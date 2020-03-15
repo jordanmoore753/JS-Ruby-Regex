aca54c9b69af71b54b5dd87bfd5fb1e7437ff07d
jordanmoore753 <jordanmoore753@gmail.com>
2020-03-15 14:21:14 -0400

    add select tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
+// default render tests
+
 test('renders select button in App', () => {
   const { getByText, getByDisplayValue } = render(<App />);
   const langSelect = getByDisplayValue('Ruby');
@@ -46,10 +48,46 @@ test('renders sections for matches in App', () => {
   expect(s2).toBeInTheDocument();
 });
 
-test('renders correct placeholder text for sections in App', () => {
+test('renders correct text for sections in App', () => {
   const { getByText } = render(<App />);
   const s1 = getByText('No results to show.'); 
   const s2 = getByText('No group results.');
   expect(s1).toBeInTheDocument();
   expect(s2).toBeInTheDocument();
 });
+
+// select option change tests
+
+test('select button values changes to JS', () => {
+  const { getByText, getByDisplayValue, queryByDisplayValue } = render(<App />);
+  const langSelect = getByDisplayValue('Ruby');
+  const e = { target: { value: 'JavaScript' }};
+
+  fireEvent.change(langSelect, e);
+  const jsSelect = getByDisplayValue('JavaScript');
+  const rbSelect = queryByDisplayValue('Ruby');
+  expect(jsSelect).toBeInTheDocument();
+  expect(rbSelect).not.toBeInTheDocument();
+});
+
+test('select button values changes to JS and back to RB', () => {
+  const { getByText, getByDisplayValue, queryByDisplayValue } = render(<App />);
+  const langSelect = getByDisplayValue('Ruby');
+  const e = { target: { value: 'JavaScript' }};
+
+  fireEvent.change(langSelect, e);
+  const jsSelect = getByDisplayValue('JavaScript');
+  const rbSelect = queryByDisplayValue('Ruby');
+
+  expect(jsSelect).toBeInTheDocument();
+  expect(rbSelect).not.toBeInTheDocument();
+
+  fireEvent.change(jsSelect, { target: { value: 'Ruby' }});
+
+  expect(getByDisplayValue('Ruby')).toBeInTheDocument();
+  expect(queryByDisplayValue('JavaScript')).not.toBeInTheDocument();
+});
+// input change tests
+
+// blur event tests
+
